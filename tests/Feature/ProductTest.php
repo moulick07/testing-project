3ba383d8e2a7308014de187315327ecc49c39b43
moulick.r <moulick.r@webcodegenie.com>
2023-10-09 18:48:44 +0530

    Product validation test cases added

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -53,7 +53,24 @@ class ProductTest extends TestCase
             ]);
             
     }
+// fetching all the Products
+public function test_for_product_fetching_all()
+{
+    $this->test_for_product_creating();
+    $api = 'api/product';
 
+
+    $response = $this->getJson($api);
+    
+    $response
+        ->assertJson([
+            'type' => "success",
+            'code' => 200,
+            'message' => 'List of Products'
+        ]);
+}
+
+//updating product
    public function test_for_product_updating()
     {
        $this->test_for_product_creating();
@@ -89,6 +106,7 @@ class ProductTest extends TestCase
             ]);
     }
 
+    //deleting product
     public function test_for_product_deleting()
     {
        $this->test_for_product_creating();
@@ -104,6 +122,79 @@ class ProductTest extends TestCase
                 'type' => "success",
                 'code' => 200,
                 'message' => 'Product deleted successfully'
+            ]);
+    }
+
+    public function test_for_product_validation_required()
+    {
+
+        $api = 'api/product';
+
+        $Product =[
+            'name' => '',
+            'short_description' => '',
+            'discounted_price'=>'',
+            'in_stock'=>'',
+            'is_active'=>'',
+            'brand'=>'',
+           
+            'main_category'=>'',
+            'category'=>'',
+           
+            'value'=>'',
+            'variant'=>'',
+            'parent_product'=>'',
+            'price'=>'',
+            'long_description'=>'',
+        ];
+
+        $response = $this->postJson($api, $Product);
+        $response
+            ->assertJson([
+                'type' => "error",
+                'code' => 422,
+                'message' => 'Server Validation Fail',
+                "errors" => [
+                    "name"=> [
+                        "Please Write a title"
+                    ],
+                    "short_description"=> [
+                        "Please write some short description"
+                    ],
+                    "long_description"=> [
+                        "The long description field is required."
+                    ],
+                    "in_stock"=> [
+                        "The in stock field is required."
+                    ],
+                    "price"=> [
+                        "The price field is required."
+                    ],
+                    "discounted_price"=> [
+                        "The discounted price field is required."
+                    ],
+                    "brand"=> [
+                        "The brand field is required."
+                    ],
+                    "category"=> [
+                        "The category field is required."
+                    ],
+                    "value"=> [
+                        "The value field is required."
+                    ],
+                    "parent_product"=> [
+                        "The parent product field is required."
+                    ],
+                    "main_category"=> [
+                        "Please add 1 main Category"
+                    ],
+                    "variant"=> [
+                        "please atleast 1 variant"
+                    ],
+                    "is_active"=> [
+                        "The is active field must be true or false."
+                    ]
+                ]
             ]);
     }
 }
