a887aa5fd9f51f55e77988c1e416b6556e61b665
moulick.r <moulick.r@webcodegenie.com>
2023-10-16 10:15:14 +0530

    Product Cateogory store and update with the new flow

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,162 +35,66 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-
-        
-        
-        // dd($request);
-            try {
+        // dd(request()->all());
+        try {
 
             DB::beginTransaction();
-                // dd($request);
+                
             $input = request()->all();
             $input['ordering'] = 1;
             $product = Product::create($input);
 
 
             //if product created than add details to product item table 
-            
             $input['product_id'] = $product->id;
-            foreach ($input['product_item'] as $key => $product_item) {
-                        $input['color'] = $product_item['color'];
-                        $input['quantity'] = $product_item['quantity'];
-                        $input['price'] = $product_item['price'];
-                        $input['final_price'] = $product_item['final_price'];
-                        $input['is_available'] = $product_item['is_available'];
-                        $input['tags'] = $product_item['tags'];
-                        $productitem = ProductItem::create($input);
-                        
-                        if($product_item){
-                            // dd($product_item['image']);
-                        $image = $product_item['image'];
-                        $files = [];
-                        foreach ($image as $file) {
-                            
+            // dd($input['product_item']);
+            foreach ($input['product_item'] as $key => $product_item)
+            {
+                
 
-                        // if ($file->extension() == 'jpeg' || $file->extension() == 'jpg' || $file->extension() == 'png') {
-                        //     $input['type'] = "image";
-                        // } else {
-                        //     $input['type'] = "video";
-                        // }
-                        // dd($file);
-
-                        $titleimage = mt_rand(3, 9) . time() . '.' . $file->extension();
-
-                        $files[] = $titleimage;
-                        $file->move(public_path('images/product_media'), end($files));
-
+                $input['color'] = $product_item['color'];
+                $input['quantity'] = $product_item['quantity'];
+                $input['price'] = $product_item['price'];
+                $input['final_price'] = $product_item['final_price'];
+                $input['is_available'] = $product_item['is_available'];
+                $input['tags'] = $product_item['tags'];
+                $productitem = ProductItem::create($input);
+                foreach ($product_item['image'] as $key=> $img) {
+                 
+                    $base64Image = file_get_contents($img);
+                    $fileName = mt_rand(3, 9) .time(). '.png'; // You can generate a unique filename with the appropriate extension.
+                    $folderPath = public_path('images/product_media'); 
+                    if (!file_exists($folderPath)) {
+                        mkdir($folderPath, 0755, true); // Create the folder if it doesn't exist.
                     }
+                    $imagePath = $folderPath . '/' . $fileName;
 
-                    foreach ($files as $key => $media) {
+                    // Save the image to the folder in the public directory.
+                    file_put_contents($imagePath, $base64Image);
+                    $product_media =  ProductMedia::create([
+                        'name'=>$fileName,
+                        "image"=>$fileName,
+                        'path'=>$folderPath,
+                        'ordering'=>$key+1,
+                        'product_item_id'=>$productitem->id,
 
-                        $input['name'] = $media;
-                        $input['image'] = $media;
-                        $input['path'] = 'images/product_media';
-                        $productmedia = ProductMedia::create($input);
-                        }
-            
-
-
-            // if ($input['color'] && $input['price'] && $input['final_price'] && $input['is_available'] && $input['tags']) {
-
-
-            //     $item_color = $input['color'];
-            //     $item_price = $input['price'];
-            //     $item_final_price = $input['final_price'];
-            //     $item_is_available = $input['is_available'];
-            //     $item_tags = $input['tags'];
-
-            //     //if the item name is coming in array then multiple values will be stored
-            //     foreach ($item_color as $key => $value) {
-                    
-            //         $input['color'] = $value;
-            //         $input['price'] = $item_price[$key];
-            //         $input['final_price'] = $item_final_price[$key];
-            //         $input['is_available'] = $item_is_available[$key];
-            //         $input['tags'] = $item_tags[$key];
-            //         // $checking = ProductItem::where('product_id', $product->id)->latest()->first();
-                    
-            //         // if ($checking) {
-                        
-            //             //     $input['ordering'] = $checking->ordering + 1;
-            //             // } else {
-            //                 //     $input['ordering'] = 1;
-            //                 // }
-                            
-                            
-            //         $product_item = ProductItem::create($input);
-                        
-            //         \Log::info($product_item);
-                    
-                   
-            //         $product_itemid[] = $product_item->id;
-                    
-            //     }
-        
-            //     if ($product_item) {
-            //         foreach ($product_itemid as $key => $itemId) {
-            //             $input['product_item_id'] = $itemId;
-            //         }
-            //         $image = $input['image'];
-            //         $files = [];
-            //         foreach ($image as $file) {
-
-            //             if ($file->extension() == 'jpeg' || $file->extension() == 'jpg' || $file->extension() == 'png') {
-            //                 $input['type'] = "image";
-            //             } else {
-            //                 $input['type'] = "video";
-            //             }
-
-
-            //             $titleimage = mt_rand(3, 9) . time() . '.' . $file->extension();
-
-            //             $files[] = $titleimage;
-            //             $file->move(public_path('images/product_media'), end($files));
-
-            //         }
-
-            //         foreach ($files as $key => $media) {
-
-            //             $input['name'] = $media;
-            //             $input['image'] = $media;
-            //             $input['path'] = 'images/product_media';
-            //             $checking = ProductMedia::where('product_item_id', $product_item->id)->latest()->first();
-            //             if ($checking) {
-
-            //                 $input['ordering'] = $checking->ordering + 1;
-            //             } else {
-            //                 $input['ordering'] = 1;
-            //             }
-            //             $productmedia = ProductMedia::create($input);
-
-            //         }
-
-
-
-            //         if ($input['itemname'] && $input['itemquantity']) {
-
-            //             $input['product_item_id'] = $product_item->id;
-            //             $item_name = $input['itemname'];
-            //             $item_quantity = $input['itemquantity'];
-
-            //             //if the itemname is coming in array then multiple values will be stored
-            //             foreach ($item_name as $key => $value) {
-            //                 $input['itemname'] = $value;
-            //                 $input['itemquantity'] = $item_quantity[$key];
-
-            //                 $product_item_size = ProductItemSize::create($input);
-            //                 // dd($value);
-            //             }
-            //         }
-            //     }
-            // }
-
-            // $product_item = ProductItem::create($input);
-
-
-            //if product item 
-
-
+                    ]);
+                
+                }
+                $testArray2=[];
+            //    dd($product_item['product_item_size']);
+                foreach ($product_item['product_item_size'] as $item_key => $itemname) {
+                // dd($itemname['id']);
+                $testArray2 =  [
+                    'itemname' => $itemname['itemname'],
+                    'itemquantity'=>$itemname['itemquantity'],
+                    'product_item_id' =>$productitem->id
+                ];
+                // dd($testArray2);
+                $productsize = ProductItemSize::create($testArray2);
+                //dd($productsize);
+                }
+            }
             DB::commit();
 
             $response = [
@@ -200,11 +104,9 @@ class ProductController extends Controller
                 'data' => $product,
                 
             ];   
-        }
-            }
+            
             return response()->json($response, 200);
-        } 
-        catch (\Throwable $th) {
+        }catch (\Throwable $th) {
             \Log::error($th);
             DB::rollBack();
             $response = [
@@ -215,6 +117,8 @@ class ProductController extends Controller
             return response()->json($response, 500);
         }
     }
+
+
 
     /**
      * Display the specified resource.
@@ -235,67 +139,164 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(StoreProductRequest $request, Product $product)
+    public function update(Request $request, Product $product)
     {
 
         try {
 
-            $input = $request->all();
+            $input = request()->all();
+            // dd($product);
             $product->update($input);
 
-            $product_item_ids = ProductItem::where('product_id', $product->id)->pluck('id');
+            $product_item_ids = ProductItem::where('product_id', $product->id)->get()->pluck('id')->toArray();
+            // $product->productItem()->productMedia()->delete();
+            //    $delete_item = ProductItemSize::whereIn('product_item_id',$product_item_ids)->delete();
+            // dd($product->id);
+            $testArray = [];
+            foreach ($input['product_item'] as $key => $product_item) {
+                $testArray['product_id'] = $product->id;
+                $testArray['color'] = $product_item['color'];
+                $testArray['quantity'] = $product_item['quantity'];
+                $testArray['price'] = $product_item['price'];
+                $testArray['final_price'] = $product_item['final_price'];
+                $testArray['is_available'] = $product_item['is_available'];
+                $testArray['tags'] = $product_item['tags'];
+                $testArray['ordering'] = $key;
+                // dd($product_item_ids);
 
-            if ($input['color']) {
-                $item_colors = $input['color'];
-                $item_prices = $input['price'];
-                $item_final_prices = $input['final_price'];
-                $item_is_availables = $input['is_available'];
-                $item_tags = $input['tags'];
+                $productitem = ProductItem::updateOrCreate(['id'=>$product_item['id']],$testArray);
+                // dd($productitem);
 
-                $product_items = ProductItem::whereIn('id', $product_item_ids)->get();
+                // dd($productitem);
+                // foreach ($product_item['image'] as $key=> $img) {
+                //     # code...
+                //     $base64Image = file_get_contents($img);
+                //     $fileName = mt_rand(3, 9) .time(). '_' . uniqid() . '.png'; // You can generate a unique filename with the appropriate extension.
+                //     $folderPath = public_path('images/product_media'); 
+                //     if (!file_exists($folderPath)) {
+                //         mkdir($folderPath, 0755, true); // Create the folder if it doesn't exist.
+                //     }
+                //     $imagePath = $folderPath . '/' . $fileName;
 
-                foreach ($product_items as $key => $product_item) {
-                    $product_item->color = $item_colors[$key];
-                    $product_item->price = $item_prices[$key];
-                    $product_item->final_price = $item_final_prices[$key];
-                    $product_item->is_available = $item_is_availables[$key];
-                    $product_item->tags = $item_tags[$key];
-                    $product_item->save();
+                //     // Save the image to the folder in the public directory.
+                //     file_put_contents($imagePath, $base64Image);
+                //     $product_media =  ProductMedia::create([
+                //         'name'=>$fileName,
+                        
+                //         'path'=>$folderPath,
+                //         'ordering'=>$key+1,
+                //         'product_item_id'=>$productitem->id,
+
+                //     ]);
+                
+                // }
+                $item_id = ProductItem::all();
+                // dd($item_id);
+
+                // dd($productitem->id);
+                $testArray2 = [];
+            foreach ($product_item['product_item_size'] as $item_key => $itemname) {
+                // dd($itemname['id']);
+                $testArray2 = [
+                    'itemname' => $itemname['itemname'],
+                    'itemquantity'=>$itemname['itemquantity'],
+                    'product_item_id' =>$productitem->id
+                ];
+                //dd($testArray2);
+                ProductItemSize::updateOrCreate(['id'=>$itemname['id']],$testArray2);
+            }
+            // dd($product_item['image']);
+            if($product_item['media']){
+                    // dd($product_item_ids[$key]);
+                //delete image first 
+                $product_media = ProductMedia::where('product_item_id',$product_item_ids[$key])->get();
+                foreach ($product_media as $productmedia) {
+                    // dd($productmedia->name);
+                    # code...
+                unlink(public_path('images/product_media/'.$productmedia->name)); 
                 }
+
+                foreach ($product_item['media'] as $key=> $img) {
+                    # code...
+                    // dd($img);
+                    $base64Image = file_get_contents($img['image']);
+                    // dd($base64Image);
+                    $fileName = mt_rand(3, 9) .time(). '.png'; // You can generate a unique filename with the appropriate extension.
+                    $folderPath = public_path('images/product_media'); 
+                    if (!file_exists($folderPath)) {
+                        mkdir($folderPath, 0755, true); // Create the folder if it doesn't exist.
+                    }
+                    $imagePath = $folderPath . '/' . $fileName;
+
+                    // Save the image to the folder in the public directory.
+                    file_put_contents($imagePath, $base64Image);
+                    $product_media =  ProductMedia::updateOrCreate([
+                        'name'=>$fileName,
+                        'image'=>$fileName,
+                        'path'=>$folderPath,
+                        'ordering'=>$key+1,
+                        'product_item_id'=>$productitem->id,
+
+                    ]);
+                
+                }
+            
             }
 
-            if ($request->hasFile('image')) {
-                $productMedias = ProductMedia::whereIn('product_item_id', $product_item_ids)->get();
+        }
 
-                $Productitleimages = $input['image'];
-                $files = [];
 
-                foreach ($Productitleimages as $key => $file) {
-                    $titleimage = mt_rand(3, 9) . time() . '.' . $file->extension();
-                    $files[] = $titleimage;
-                    $file->move(public_path('images/product_media'), $titleimage);
-                }
+            // if ($input['color']) {
+            //     $item_colors = $input['color'];
+            //     $item_prices = $input['price'];
+            //     $item_final_prices = $input['final_price'];
+            //     $item_is_availables = $input['is_available'];
+            //     $item_tags = $input['tags'];
 
-                foreach ($productMedias as $key => $mediaupdate) {
-                    $mediaupdate->name = $files[$key];
-                    $mediaupdate->image = $files[$key];
-                    $mediaupdate->path = 'images/product_media';
-                    $mediaupdate->save();
-                }
-            }
+            //     $product_items = ProductItem::whereIn('id', $product_item_ids)->get();
 
-            if ($input['itemname'] && $input['itemquantity']) {
-                $item_names = $input['itemname'];
-                $item_quantities = $input['itemquantity'];
+            //     foreach ($product_items as $key => $product_item) {
+            //         $product_item->color = $item_colors[$key];
+            //         $product_item->price = $item_prices[$key];
+            //         $product_item->final_price = $item_final_prices[$key];
+            //         $product_item->is_available = $item_is_availables[$key];
+            //         $product_item->tags = $item_tags[$key];
+            //         $product_item->save();
+            //     }
+            // }
 
-                $product_item_sizes = ProductItemSize::whereIn('product_item_id', $product_item_ids)->get();
+            // if ($request->hasFile('image')) {
+            //     $productMedias = ProductMedia::whereIn('product_item_id', $product_item_ids)->get();
 
-                foreach ($product_item_sizes as $key => $item_size) {
-                    $item_size->itemname = $item_names[$key];
-                    $item_size->itemquantity = $item_quantities[$key];
-                    $item_size->save();
-                }
-            }
+            //     $Productitleimages = $input['image'];
+            //     $files = [];
+
+            //     foreach ($Productitleimages as $key => $file) {
+            //         $titleimage = mt_rand(3, 9) . time() . '.' . $file->extension();
+            //         $files[] = $titleimage;
+            //         $file->move(public_path('images/product_media'), $titleimage);
+            //     }
+
+            //     foreach ($productMedias as $key => $mediaupdate) {
+            //         $mediaupdate->name = $files[$key];
+            //         $mediaupdate->image = $files[$key];
+            //         $mediaupdate->path = 'images/product_media';
+            //         $mediaupdate->save();
+            //     }
+            // }
+
+            // if ($input['itemname'] && $input['itemquantity']) {
+            //     $item_names = $input['itemname'];
+            //     $item_quantities = $input['itemquantity'];
+
+            //     $product_item_sizes = ProductItemSize::whereIn('product_item_id', $product_item_ids)->get();
+
+            //     foreach ($product_item_sizes as $key => $item_size) {
+            //         $item_size->itemname = $item_names[$key];
+            //         $item_size->itemquantity = $item_quantities[$key];
+            //         $item_size->save();
+            //     }
+            // }
 
             return [
                 'type' => 'success',
@@ -345,3 +346,4 @@ class ProductController extends Controller
         }
     }
 }
+
